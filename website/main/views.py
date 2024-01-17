9bf72bbeb96200df9904bdab43d3f1bae655c1ca
unknown <zakaria.janah2018@gmail.com>
2024-01-17 19:09:41 +0100

    adding templates connectionToDatabase signup_login_forms

---
+++ website/main/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .forms import RegisterForm
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'main/home.html')
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+    else:
+        form = RegisterForm()
+
+    return render(request, 'registration/sign_up.html', {"form":form})
