4c36285e47afde4d331c48a82f470c20242d072b
unknown <zakaria.janah2018@gmail.com>
2024-01-18 19:23:06 +0100

    ban users

--- website/main/views.py
+++ website/main/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm, PostForm
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User, Group
 from .models import Post
 # Create your views here.
 
@@ -11,14 +12,32 @@ def home(request):
 
     if request.method == "POST":
         post_id = request.POST.get("post-id")
-        post = Post.objects.filter(id=post_id).first()
-        if post and post.author == request.user:
-            post.delete()
+        user_id = request.POST.get("user-id")
 
+        if post_id:
+            post = Post.objects.filter(id=post_id).first()
+            if post and (post.author == request.user or request.user.has_perm("main.delete_post")):
+                post.delete()
+
+        elif user_id:
+            user = User.objects.filter(id=user_id).first()
+            if user and request.user.is_staff:
+                try:
+                    group = Group.objects.get(name="default")
+                    group.user_set.remove(user)
+                except:
+                    pass
+
+                try:
+                    group = Group.objects.get(name="mod")
+                    group.user_set.remove(user)
+                except:
+                    pass
 
     return render(request, 'main/home.html', {"posts":posts})
 
 @login_required(login_url="/login")
+@permission_required("main.add_post", login_url='/login', raise_exception=True)
 def create_post(request):
     if request.method == "POST":
         form = PostForm(request.POST)
